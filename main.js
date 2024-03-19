4be34184ab3dd9f1f5d03da77ade9f2e39b0dfb5
revesfideles <revesfideles@gmail.com>
2024-03-19 15:24:13 -0400

    completed lab

--- main.js
+++ main.js
@@ -1,12 +1,54 @@
 // Defining text characters for the empty and full hearts for you to use later.
-const EMPTY_HEART = '♡'
-const FULL_HEART = '♥'
+const EMPTY_HEART = '♡';
+const FULL_HEART = '♥';
 
 // Your JavaScript code goes here!
 
+document.addEventListener('DOMContentLoaded', () => {
+  const likeGlyphs = document.querySelectorAll('.like-glyph');
 
+  likeGlyphs.forEach((likeGlyph) => {
+    likeGlyph.addEventListener('click', (event) => {
+      const errorModal = document.getElementById('modal');
+      const errorMessage = document.getElementById('modal-message');
 
+      mimicServerCall()
+        .then(() => {
+          if (likeGlyph.classList.contains('empty-heart')) {
+            likeGlyph.innerHTML = FULL_HEART;
+            likeGlyph.classList.remove('empty-heart');
+            likeGlyph.classList.add('activated-heart');
+          } else {
+            likeGlyph.innerHTML = EMPTY_HEART;
+            likeGlyph.classList.add('empty-heart');
+            likeGlyph.classList.remove('activated-heart');
+          }
+        })
+        .catch(() => {
+          errorModal.classList.remove('hidden');
+          errorMessage.textContent = 'Server Error';
 
+          setTimeout(() => {
+            errorModal.classList.add('hidden');
+          }, 3000);
+        });
+    });
+  });
+});
+
+function mimicServerCall() {
+  return new Promise((resolve, reject) => {
+    // Simulating a server response
+    const randomNum = Math.random();
+    setTimeout(() => {
+      if (randomNum < 0.5) {
+        resolve();
+      } else {
+        reject();
+      }
+    }, 1000);
+  });
+}
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
